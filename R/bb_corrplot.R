b575c55de5a780b567aa6617f9cce8678f5137fc
Jeff Parks <36110645+jefedigital@users.noreply.github.com>
2023-06-29 17:51:36 -0400

    bb_corrplot vertical xlabs, added titles

--- R/bb_corrplot.R
+++ R/bb_corrplot.R
@@ -2,14 +2,19 @@
 #' of the numeric columns using corrr
 #'
 #' @param df a dataframe
+#' @param title a title
 #' @return a corrr rplot
 #' @export
-bb_corrplot <- function(df){
+bb_corrplot <- function(df,title=NULL){
 
   corr_tbl <- df[,sapply(df, is.numeric)] %>%
     correlate() %>%
     rearrange() %>%
     shave(upper=TRUE)
 
-  return(rplot(corr_tbl))
+  corr_plot <- rplot(corr_tbl) +
+    ggtitle({{title}}) +
+    theme(axis.text.x = element_text(angle=90, vjust=.5, hjust=1))
+
+  return(corr_plot)
 }
