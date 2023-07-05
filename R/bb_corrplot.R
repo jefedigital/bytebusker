5c92c559ec1be80417623d2cddf0207c276885b2
Jeff Parks <36110645+jefedigital@users.noreply.github.com>
2023-07-05 19:33:09 -0400

    + threshold to bb_corrplot

--- R/bb_corrplot.R
+++ R/bb_corrplot.R
@@ -3,13 +3,17 @@
 #' Takes a dataframe and makes a nice correlation plot of the numeric columns using corrr
 #'
 #' @param df a dataframe
+#' @param threshold a threshold 0-1 to filter absolute results
 #' @param title a title
 #' @return a corrr rplot
 #' @export
-bb_corrplot <- function(df,title=NULL){
+bb_corrplot <- function(df, threshold=0, title=NULL){
+
+  title <- str_glue(title, ' - threshold:', threshold)
 
   corr_tbl <- df[,sapply(df, is.numeric)] %>%
     correlate() %>%
+    mutate_if(is.numeric, ~ ifelse(abs(.x) < threshold, 0, .x)) %>%
     rearrange() %>%
     shave(upper=TRUE)
 
