5e4d7b327ebeacd42510de66e06dfc14d1023262
Jeff Parks <36110645+jefedigital@users.noreply.github.com>
2023-07-05 20:21:18 -0400

    + threshold to bb_corrplot

--- R/bb_corrplot.R
+++ R/bb_corrplot.R
@@ -7,17 +7,27 @@
 #' @param title a title
 #' @return a corrr rplot
 #' @export
-bb_corrplot <- function(df, threshold=0, title=NULL){
+bb_corrplot <- function(df, threshold=0, title='Corrplot'){
 
   title <- str_glue(title, ' - threshold:', threshold)
 
-  corr_tbl <- df[,sapply(df, is.numeric)] %>%
-    correlate() %>%
-    mutate_if(is.numeric, ~ ifelse(abs(.x) < threshold, 0, .x)) %>%
-    rearrange() %>%
-    shave(upper=TRUE)
+  remove_cols_rows <- function(tbl) {
+    cols <- which(apply(tbl == 0 | is.na(tbl), 2, all))
+    rows <- which(apply(tbl[,-1] == 0 | is.na(tbl)[,-1], 1, all))
+    return(list(cols=cols,rows=rows))
+  }
 
-  corr_plot <- rplot(corr_tbl) +
+  corr_tbl <- df %>%
+    correlate() %>%
+    mutate_if(is.numeric, ~ ifelse(abs(.x) < {{threshold}}, 0, .x)) %>%
+    rearrange()
+
+  remove <- remove_cols_rows(corr_tbl)
+  reduced_corr_tbl <- corr_tbl[-remove$rows, -remove$cols]
+
+  corr_plot <- reduced_corr_tbl %>%
+    shave(upper=TRUE) %>%
+    rplot() +
     ggtitle({{title}}) +
     theme(axis.text.x = element_text(angle=90, vjust=.5, hjust=1))
 
