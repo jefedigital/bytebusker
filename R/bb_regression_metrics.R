363e9e3e807991c7aa608e4eced00808409f0f7f
Jeff Parks <36110645+jefedigital@users.noreply.github.com>
2023-07-07 13:01:04 -0400

    colname

--- R/bb_regression_metrics.R
+++ R/bb_regression_metrics.R
@@ -5,11 +5,12 @@
 #' @param pred a vector of predicted values
 #' @param obs a vector of observed values
 #' @param cap an optional caption
+#' @param colname an optional column label
 #' @param dig optional number of decimal places for rounding - default 3
 #' @param fw optional boolean T/F for full-width - default F
 #' @return a kable-formatted table
 #' @export
-bb_regression_metrics <- function(pred, obs, dig=3, cap=NA){
+bb_regression_metrics <- function(pred, obs, dig=3, cap=NA, colname='metrics'){
 
   obs <- as.vector(obs)
   pred <- as.vector(pred)
@@ -20,7 +21,7 @@ bb_regression_metrics <- function(pred, obs, dig=3, cap=NA){
   mape <- mape_vec(obs, pred, na_rm = TRUE)
 
   df <- as.data.frame(c(rsq=rsq, rmse=rmse, mae=mae, mape=mape))
-  names(df) <- 'metrics'
+  names(df) <- colname
 
   return(bb_table(df, dig={{dig}}, cap={{cap}}))
 }
