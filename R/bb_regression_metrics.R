e649d7cc92bd0a0ff724ba043339e7fce5bee5ef
Jeff Parks <36110645+jefedigital@users.noreply.github.com>
2023-07-07 13:09:49 -0400

    bb_regression_metrics

--- R/bb_regression_metrics.R
+++ R/bb_regression_metrics.R
@@ -1,27 +1,24 @@
 #' bb_regression_metrics
 #'
-#' Takes vectors of predictions and observations from regression models, returns common error metrics in a table using yardstick.
+#' Takes vectors of predictions and observations from regression models, returns common error metrics in a df using yardstick.
 #'
 #' @param pred a vector of predicted values
 #' @param obs a vector of observed values
-#' @param cap an optional caption
 #' @param colname an optional column label
-#' @param dig optional number of decimal places for rounding - default 3
-#' @param fw optional boolean T/F for full-width - default F
-#' @return a kable-formatted table
+#' @return a df
 #' @export
-bb_regression_metrics <- function(pred, obs, dig=3, cap=NA, colname='metrics'){
+bb_regression_metrics <- function(pred, resp, colname='metrics'){
 
-  obs <- as.vector(obs)
+  obs <- as.vector(resp)
   pred <- as.vector(pred)
 
-  rmse <- rmse_vec(obs, pred, na_rm = TRUE)
-  mae <- mae_vec(obs, pred, na_rm = TRUE)
-  rsq <- rsq_vec(obs, pred, na_rm = TRUE)
-  mape <- mape_vec(obs, pred, na_rm = TRUE)
+  rmse <- rmse_vec(resp, pred, na_rm = TRUE)
+  mae <- mae_vec(resp, pred, na_rm = TRUE)
+  rsq <- rsq_vec(resp, pred, na_rm = TRUE)
+  mape <- mape_vec(resp, pred, na_rm = TRUE)
 
   df <- as.data.frame(c(rsq=rsq, rmse=rmse, mae=mae, mape=mape))
   names(df) <- colname
 
-  return(bb_table(df, dig={{dig}}, cap={{cap}}))
+  return(df)
 }
